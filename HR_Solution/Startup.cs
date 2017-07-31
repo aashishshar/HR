51a325d983cb216b69bfb761c7e862d62af18792
aashishshar <aashishshar@gmail.com>
2017-07-31 15:23:00 +0530

    ft

---
+++ HR_Solution/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HR_Solution.Startup))]
+namespace HR_Solution
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
